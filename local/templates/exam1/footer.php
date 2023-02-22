0528e7c41f4b3f3313d1a336db4ebb2d803e74cb
Егор Самсонкин <miikkiivvii@gmail.com>
2023-02-22 17:48:28 +0300

    ex1-6

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -7,6 +7,26 @@ IncludeTemplateLangFile(__FILE__);
                 <!-- /content -->
                 <!-- side -->
                 <div class="side">
+                    <!-- side menu -->
+                    <!-- side menu -->
+                    <?$APPLICATION->IncludeComponent(
+                        "bitrix:menu",
+                        "left",
+                        Array(
+                            "ALLOW_MULTI_SELECT" => "N",
+                            "CHILD_MENU_TYPE" => "left",
+                            "DELAY" => "N",
+                            "MAX_LEVEL" => "1",
+                            "MENU_CACHE_GET_VARS" => array(""),
+                            "MENU_CACHE_TIME" => "3600",
+                            "MENU_CACHE_TYPE" => "N",
+                            "MENU_CACHE_USE_GROUPS" => "Y",
+                            "ROOT_MENU_TYPE" => "left",
+                            "USE_EXT" => "Y"
+                        )
+                    );?>
+                    <!-- /side menu -->
+                    <!-- /side menu -->
                     <!-- side anonse -->
                     <div class="side-block side-anonse">
                         <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
